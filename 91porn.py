45efeaf2ac0e0afcce4c172b37c619b8bb44485a
Rokate2021 <77772464+Rokate@users.noreply.github.com>
2021-03-16 16:49:27 +0800

    Update 91porn.py

--- 91porn.py
+++ 91porn.py
@@ -2,7 +2,12 @@ import os
 
 url_91 = os.getenv('URL91')
  
-if url_91 == 'https://0316.workarea2.live/v.php?next=watch' :
- 
-   print(true)
+def main():
+    if url_91 == 'https://0316.workarea2.live/v.php?next=watch':
+        print(111)
+
+
+if __name__ == '__main__':
+    main()
+
 
