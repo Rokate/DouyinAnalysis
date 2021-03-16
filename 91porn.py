f7082c80933a5fbeb092938cfdd75b840f8736e6
Rokate2021 <77772464+Rokate@users.noreply.github.com>
2021-03-16 16:01:31 +0800

    Update 91porn.py

--- 91porn.py
+++ 91porn.py
@@ -1,34 +1,4 @@
-import requests
-import re
 import os
 
 url_91 = os.getenv('secrets.URL91')
 print(url_91)
-headers = {
-    'User-Agent': " Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
-
-}
-data = {"session_language": "cn_CN"}
-
-
-def getcontext():
-    r = requests.post(url_91, data=data)
-    url = re.findall(
-        r'<a href="(.*?)&page=1&viewtype=basic&category=mr', r.text)
-    img = re.findall(r'<img class="img-responsive" src="(.*?).jpg" />', r.text)
-    time = re.findall(r'<span class="duration">(.*?)</span>', r.text)
-    title = re.findall(
-        r' <span class="video-title title-truncate m-t-5">(.*?)</span>', r.text)
-    title_91 = "# 91PORN每日最新视频列表" + "\n"
-    fp = open('remark.md', 'a')
-    fp.write(title_91)
-    for i in range(len(img)):
-        #  list = 'url:' + url[i] + ' imgurl:' + img[i] + '.jpg time:' + time[i] + ' title:' + title[i]
-        MD = '![Img](' + img[i] + '.jpg' + ')' + '\n' + '## 时长:' + time[i] + ' [' + title[i] + '](' + url[
-            i] + ')' + '\n'
-        fp.write(MD)
-    fp.close()
-
-
-if __name__ == "__main__":
-    getcontext()
